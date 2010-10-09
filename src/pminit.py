70f7b6629698984de09ba9b70c3390ce9204f8d0
Jookia <166291@gmail.com>
2010-10-10 10:09:29 +1100

    Half-working code.

--- src/pminit.py
+++ src/pminit.py
@@ -19,6 +19,7 @@ import os
 # Init
 # -------------------- #
 rules = list()
+configFile = ""
 
 # -------------------- #
 # Functions
@@ -36,6 +37,9 @@ def add_config( opt, optstr, var, parser ):
 	"""Add a rule to the configuration."""
 	rules.append( var )
 
+def set_config_file( opt, optstr, var, parser ):
+	configFile = var
+
 # -------------------- #
 # OptionParser
 # -------------------- #
@@ -43,6 +47,9 @@ parser = OptionParser()
 
 parser.add_option( "-v", "--version", dest = "version", action = "store_true",
 	default = False, help = "show version info and exit" )
+parser.add_option( "-f", "--set-config-file",
+	callback = set_config_file, action = "callback", type = str,
+	help = "Set the configuration file." )
 parser.add_option( "-c", "--add-config",
 	callback = add_config, action = "callback", type = str,
 	help = "Add additional configuration without editing config file." )
@@ -51,19 +58,20 @@ parser.add_option( "-c", "--add-config",
 # Run
 # -------------------- #
 if( __name__ == "__main__" ):
+	( options, args ) = parser.parse_args()
+	
 	conf = pmconfig.PMConfig()
 	
 	confpaths = [
 		"/etc/pymate.conf",
-		"~/.config/pymate/pymate.conf",
-		os.path.dirname( __file__ ) + "/../pymate.conf"
+		"~/.config/pymate/pymate.conf"
 	]
+	
+	confpaths.append( configFile )
 	
 	for path in confpaths:
 		if( os.path.isfile( path ) ):
 			conf.parseFile( path )
-	
-	( options, args ) = parser.parse_args()
 	
 	conf.args = args
 	conf.opts = options
